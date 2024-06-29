39c76d107116c2f94c77fea3cb34fa8e9db931bc
Jaqueline Torres <jaque@MacBook-Pro.local>
2024-06-28 21:24:44 -0600

    modified: files CSS, JS, HTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,22 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('#name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('location');
+/* Se toma el nombre del HTML*/
+const $n = document.getElementById("name").innerHTML;
 
-/**
- * wait necesita una función asyc
- */
-async function displayUser(username) {
+// Obtener los elementos del DOM donde queremos mostrar los datos
+const userNameElement = document.getElementById('user-name');
+const userAvatarElement = document.getElementById('user-avatar');
+
+/* const $b = document.querySelector('.blog');
+const $l = document.querySelector('.location'); */
+
+const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `${baseEndpoint}/users/${$n}`;
+
+/* wait necesita una función asyc */
+/* async function displayUser(url) {
 
   try {
     $n.textContent = 'cargando...';
-    const response = await fetch(`${usersEndpoint}/${username}`);
+    const response = await fetch(url);
     console.log(data);
     $n.textContent = '${data.name}';
     $b.textContent = '${data.blog}';
@@ -20,7 +25,7 @@ async function displayUser(username) {
     console.error(`Algo salió mal: ${error}`);
   }
 
-}
+} */
 
 /* function handleError(err) {
   console.log('OH NO!');
@@ -28,4 +33,22 @@ async function displayUser(username) {
   err.textContent = `Algo salió mal: ${err}`
 }
 */
-displayUser('stolinski'); 
+//displayUser(baseEndpoint); 
+
+
+/* Se refactorizó la función displayUser */
+const displayUser = async (url) => {
+  try {
+    //console.log(url);
+    const resolve = await fetch(url);
+    const data = await resolve.json();
+    //console.log(data.name);
+    //console.log(data.avatar_url);
+    userNameElement.textContent = data.name;
+    userAvatarElement.src = data.avatar_url;
+  } catch (exception) {
+    console.error(exception)
+  }
+};
+
+displayUser(usersEndpoint);
